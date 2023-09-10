9846364b8ecc2466b2481a41eda192c7d3767b18
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-09-10 18:17:41 +0300

    Осталось поправить метод вывода на экран

--- notes.py
+++ notes.py
@@ -18,7 +18,7 @@ def show_main_menu() -> int:
     return choice
 
 def read_csv(filename: str) -> list:
-    '''Считывание csv-файла в список словарей'''
+    '''Считывание csv-файла с заметками'''
     data = []
     fields = ["Идентификатор", "Заголовок", "Тело", "Дата создания","Дата изменения"]
     with open(filename, 'r', encoding='utf-8') as fin:
@@ -28,7 +28,7 @@ def read_csv(filename: str) -> list:
     return data
 
 def write_csv(filename: str, data: list):
-    '''Запись в csv-файл списка словарей'''
+    '''Запись в csv-файл заметок'''
     with open(filename, 'w', encoding='utf-8') as fout:
         for i in range(len(data)):
             s = ''
@@ -36,10 +36,11 @@ def write_csv(filename: str, data: list):
                 s += v + ';'
             fout.write(f'{s[:-1]}\n')
 
-def get_formatted_result(data: list) -> str:
+def print_result(data: list):
     '''Выводит список в виде оформленной, отсортированной по дате изменения, таблицы'''
     if len(data) == 0:
-        return "Список пуст"
+        print("Список пуст")
+        return 
     # Формируем список максимальных длин полей (с учетом заголовка)
     lens = []
     for key in data[0].keys():
@@ -60,16 +61,7 @@ def get_formatted_result(data: list) -> str:
         s += "│" + "│".join(vals) + "│"  + "\n"
     # Обрамляем таблицу снизу        
     s += "└" + "┴".join(lines) + "┘"
-    return s
-
-def print_result(data: list):
-    '''Вывод таблицы с данными в консоль'''
-    print(get_formatted_result(data))
-
-def get_search_title() -> str:
-    '''Функция для ввода фразы для поиска в заголовке'''
-    title = input("Введите часть заголовка для поиска: ")
-    return title
+    print(s)
 
 def find_by_title(data: list, title: str) -> list:
     '''Функция поиска указанной фразы в заголовке.
@@ -80,26 +72,28 @@ def find_by_title(data: list, title: str) -> list:
             res.append(i)
     return res
 
-def get_search_date() -> str:
-    '''Функция для ввода даты для поиска'''
-    date = input("Введите часть даты создания или изменения для поиска: ")
-    return date
-
 def find_by_date(data: list, date: str) -> list:
     '''Функция поиска по дате создания/изменения.
        Выдает список словарей для всех подходящих заметок'''
     res = []
     for i in data:
-        if date.lower in i["Дата создания"] or date.lower in i["Дата изменения"]:
+        if date.lower() in i["Дата создания"] or date.lower() in i["Дата изменения"]:
             res.append(i)
     return res
 
-def get_new_note(data: list) -> dict:
+def find_by_id(notes: list, id: str) -> dict:
+    '''Функция поиска по идентификатору'''
+    for note in notes:
+        if id == note["Идентификатор"]:
+            return note
+    return None
+
+def get_new_note(notes: list) -> dict:
     '''Функция для создания новой заметки'''
     res = {}
     ids = []
-    for i in data:
-        ids.append(int(i["Идентификатор"]))
+    for notes in notes:
+        ids.append(int(notes["Идентификатор"]))
     res["Идентификатор"] = str(max(ids) + 1)
     res["Заголовок"] = input(f"Введите заголовок: ")
     res["Тело"] = input(f"Введите текст заметки: ")
@@ -107,70 +101,57 @@ def get_new_note(data: list) -> dict:
     res["Дата изменения"] = str(datetime.datetime.now())
     return res
 
-def add_note(data: list, note: dict):
-    '''Метод для добавления новой заметки в базу'''
-    data.append(note)
-    print_result([note])
-    print("Новая заметка успешно добавлена")
+def delete_note(notes: list, id: str):
+    '''Метод для удаления заметки'''
+    note = find_by_id(notes, id)
+    if note == None:
+        print("Заметки с таким Id не найдено")    
+        return
+    notes.remove(note)
 
-def delete_note(data: list, name: str):
-    '''Метод для удаления контакта из справочника'''
-    users_for_delete = find_by_title(data, name)
-    if len(users_for_delete) == 0:
-        print("По вашему запросу не найдено контактов")
+def change_note(notes: list, id: str):
+    '''Метод для редактирования заметки'''
+    note = find_by_id(notes, id)
+    if note == None:
+        print("Заметки с таким Id не найдено")
         return
-    print("Контакты для удаления:")
-    print_result(users_for_delete)
-    d = input("Удалить указанные контакты? (да/нет): ")
-    if d.lower() == "да":
-        for user in users_for_delete:
-            data.remove(user)
-            print(f"Контакт {user['Фамилия']} {user['Имя']} удален из справочника.")
-
-def change_user(data: list, name: str):
-    '''Метод для редактирования данных контакта в справочнике'''
-    old_user_data = find_by_title(data, name)
-    if len(old_user_data) == 0:
-        print("По вашему запросу не найдено контактов")
-        return
-    elif len(old_user_data) > 1:
-        print("По вашему запросу найдено несколько контактов, формулируйте запрос точнее")
-        return
-    print("Данные контакта для редактирования:")
-    print_result(old_user_data)
+    delete_note(notes, id)
     print("Введите новые данные. Если поле не требует изменения, оставьте его пустым")
-    new_user_data = get_new_note(old_user_data[0].keys())
-    for key in new_user_data.keys():
-        if new_user_data[key] == "":
-            new_user_data[key] = old_user_data[0][key]
-    data.remove(old_user_data[0])
-    data.append(new_user_data)
+    s = input(f"Введите новый заголовок: ")
+    if len(s) > 0:
+        note["Заголовок"] = s
+    s = input(f"Введите новый текст заметки: ")
+    if len(s) > 0:
+        note["Тело"] = s
+    note["Дата изменения"] = str(datetime.datetime.now())
+    notes.append(note)
     print("Контакт успешно отредактирован")
 
 def work_with_notes():
-    choice = show_main_menu()
     notes = read_csv('notes.csv')
+    choice = show_main_menu()
     while (choice != 7):
         cls()
         if choice == 1:
             print_result(notes)
         elif choice == 2:
-            title = get_search_title()
+            title = input("Введите часть заголовка для поиска: ")
             print_result(find_by_title(notes, title))
         elif choice == 3:
-            date = get_search_date()
+            date = input("Введите часть даты создания или изменения для поиска: ")
             print_result(find_by_date(notes, date))
         elif choice == 4:
             note_data = get_new_note(notes)
-            add_note(notes, note_data)
+            notes.append(note_data)
+            print("Новая заметка успешно добавлена")
             write_csv('notes.csv', notes)
         elif choice == 5:
-            title = get_search_title()
-            change_user(notes, title)
+            id = input("Введите Id редактируемой заметки заметки: ")
+            change_note(notes, id)
             write_csv('notes.csv', notes)
         elif choice == 6:
-            title = get_search_title()
-            delete_note(notes, title)
+            id = input("Введите Id удаляемой заметки: ")
+            delete_note(notes, id)
             write_csv('notes.csv', notes)
         choice = show_main_menu()
 
